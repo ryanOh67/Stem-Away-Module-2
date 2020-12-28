695f4b3b8dde54c70aff31bd6854d4def8478f90
ryanoh0303 <42789996+ryanoh0303@users.noreply.github.com>
2020-12-27 20:34:39 -0800

    test1234

---
+++ module3.py
@@ -0,0 +1,17 @@
+import logging
+import pandas as pd
+import numpy as np
+from numpy import random
+import gensim
+import nltk
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+from nltk.corpus import stopwords
+import re
+from bs4 import BeautifulSoup
+
+data = pd.read_csv('helloRyan.csv')
+data.head()
+print(data['Date'][2])
