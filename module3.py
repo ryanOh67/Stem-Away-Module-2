691497a15f734366cf5ce42b5ad6d61bfc7788ca
ryanoh0303 <42789996+ryanoh0303@users.noreply.github.com>
2020-12-27 20:51:43 -0800

    df

--- module3.py
+++ module3.py
@@ -6,4 +6,7 @@ data = pd.read_csv('helloRyan.csv')
 data.head()
 print('hi')
 print(data['Date'][2])
+df = data.copy()
+print(df)
+
 # go to the bug thing, stage change, and look at the blue bar on the botrtom of the  screen to push changes
