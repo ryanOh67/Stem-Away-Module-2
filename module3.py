d9c058e55c733cc9c1da8a836a8396429d22751b
ryanoh0303 <42789996+ryanoh0303@users.noreply.github.com>
2020-12-28 21:51:27 -0800

    change

--- module3.py
+++ module3.py
@@ -7,6 +7,7 @@ data.head()
 print('hi')
 print(data['Date'][2])
 df = data.copy()
-print(df)
+test_df = df.copy()
+print_plot(10)
 
 # go to the bug thing, stage change, and look at the blue bar on the botrtom of the  screen to push changes
