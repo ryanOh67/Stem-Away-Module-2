2ee42da31815c82fdb7547dd59a8f262a459f63c
ryanoh0303 <42789996+ryanoh0303@users.noreply.github.com>
2021-01-10 15:55:52 -0800

    finished the module need to revise

--- module3.py
+++ module3.py
@@ -17,9 +17,9 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.metrics import f1_score
-
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report
-
+from sklearn.linear_model import SGDClassifier
 #%matplotlib inline
 
 
@@ -29,37 +29,80 @@ data.head()
 df = data.copy()
 X = df.Likes
 y = df.LengthofContent
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state = 42) #when I plug in 0.5 for the test size, it works with values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state = 50) #when I plug in 0.5 for the test size, it works with values
 
-X_train.head()  #line 113
+X_train.head()  
 y_train.head()
 
-#print(y_train)
-#categorize to long, medium short?
-nb = Pipeline([('vect', CountVectorizer(lowercase=False)),
+nb = Pipeline([('vect', CountVectorizer()),
                ('tfidf', TfidfTransformer()),
                ('clf', MultinomialNB()),
               ])
 nb.fit(X_train, y_train)
 
-
-
 y_pred = nb.predict(X_test)
-
-#relationship between the length of the post and the likes it gets
-
 my_categories = ['Short', 'Medium', 'Long']
 
-#'38','208','356','60', '1504','861','27','215'
-print('accuracy %s' % accuracy_score(y_pred, y_test))
+
+#print('accuracy %s' % accuracy_score(y_pred, y_test))
 res1311 = accuracy_score(y_pred, y_test)
-#print(len(y_pred))
-#print(len(y_test))
-#print(len(y_train))
-print(classification_report(y_test, y_pred,labels =['Short', 'Medium', 'Long'], target_names=my_categories)) #define my_categories
-#labels=[1, 2, 3,4,5,6,7,8,9,10,11]
+
+#print(classification_report(y_test, y_pred,labels =['Short', 'Medium', 'Long'], target_names=my_categories)) #define my_categories
 
 
 
+
+
+sgd = Pipeline([('vect', CountVectorizer()),
+                ('tfidf', TfidfTransformer()),
+                 ('clf', SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, random_state=42, max_iter=5, tol=None)),
+               ])
+
+sgd.fit(X_train, y_train)
+
+y_pred = sgd.predict(X_test)
+
+
+#print('accuracy %s' % accuracy_score(y_pred, y_test))
+res1321 = sgd.score(y_pred, y_test)
+
+#print(classification_report(y_test, y_pred,labels =['Short', 'Medium', 'Long'],target_names=my_categories))
+
+
+
+
+
+
+
+from sklearn.linear_model import LogisticRegression
+
+logreg = Pipeline([('vect', CountVectorizer()),
+                ('tfidf', TfidfTransformer()),
+                ('clf', LogisticRegression(n_jobs=1, C=1e5)),
+               ])
+logreg.fit(X_train, y_train)
+
+y_pred = logreg.predict(X_test)
+res2331 = logreg.score(y_pred, y_test)
+
+
+
+
+dtree = Pipeline([('vect', CountVectorizer()),
+                ('tfidf', TfidfTransformer()),
+                ('clf', DecisionTreeClassifier(random_state=0)),
+               ])
+dtree.fit(X_train, y_train)
+
+y_pred = dtree.predict (X_test)
+res1341 = accuracy_score(y_pred, y_test)
+#print(classification_report(y_test, y_pred,labels =['Short', 'Medium', 'Long'],target_names=my_categories))
+
+results = pd.DataFrame({'Model': ['Naive Bayes MultinomialNB', 'Linear SVM', 'Logistic Regression', 'Decision Tree'],
+                         'Accuracy': [res1311,res1341, res1321, res2331]})
+results.set_index('Model')
+results.sort_values(by='Accuracy')
+print(results)
+ 
 # go to the bug thing, stage change, and look at the blue bar on the botrtom of the  screen to push changes
-#recommends the length of the post?z
+#recommends the length of the post?
